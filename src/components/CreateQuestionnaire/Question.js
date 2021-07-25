9bc0f4c7ac314f1f6bb455a41b81b738348749da
Tal Arbatov <talarbatov98@gmail.com>
2021-07-26 02:19:38 +0300

    code cleanup

--- src/components/CreateQuestionnaire/Question.js
+++ src/components/CreateQuestionnaire/Question.js
@@ -4,7 +4,6 @@ import Option from './Option';
 import TextField from '@material-ui/core/TextField';
 import { Button, CardContent } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
-import Typography from '@material-ui/core/Typography';
 const Question = ({
   submitQuestion,
   questionNum
@@ -12,33 +11,16 @@ const Question = ({
   const [name, setName] = useState('');
   const [options, setOptions] = useState([]);
 
-  console.log(name, options);
-
   const handleQuestionSubmition = () => {
     submitQuestion(name, options);
     setName('');
     setOptions([]);
   };
+  
   return (
-    // <div> 
-    //   {/* <label>Enter Question Name ({ questionNum + 1 }):</label> */}
-    //   <TextField id="outlined-basic" label={ `Enter Question Name (${ questionNum + 1 })` } variant="outlined" onChange={ e => setName(e.target.value) } value={ name }/>
-
-    //   {/* <input type="text" onChange={ e => setName(e.target.value) } value={ name }></input> */}
-    //   <br />
-    //   {/* <button onClick={ () => setOptions({ ...options.options, amount: options.amount + 1 }) }>add option</button> */}
-    //   <Option createOption={ option => setOptions([...options, option]) } optionsNum={ options.length }/>
-    //   <button onClick={ () => handleQuestionSubmition() }>Submit question</button>
-    // </div>
-
     <Card className="question-wrapper" variant="outlined">
     <CardContent className="questionnaire-wrapper2" >
-      {/* <label>Enter Question Name ({ questionNum + 1 }):</label> */}
       <TextField id="outlined-basic" className="question-input" label={ `Enter Question Name (${ questionNum + 1 })` } variant="outlined" onChange={ e => setName(e.target.value) } value={ name }/>
-
-      {/* <input type="text" onChange={ e => setName(e.target.value) } value={ name }></input> */}
-      <br />
-      {/* <button onClick={ () => setOptions({ ...options.options, amount: options.amount + 1 }) }>add option</button> */}
       <Option createOption={ option => setOptions([...options, option]) } optionsNum={ options.length }/>
       <Button className="add-question-btn" variant="contained" color="primary" onClick={ handleQuestionSubmition }>Add Question</Button>
     </CardContent>
--- src/components/CreateQuestionnaire/Questionnaire.js
+++ src/components/CreateQuestionnaire/Questionnaire.js
@@ -7,6 +7,7 @@ import Title from './Title';
 import { Button, CardContent } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
+
 const Questionnaire = ({
   createQuestionnaire
 }) => {
@@ -17,36 +18,23 @@ const Questionnaire = ({
     setQuestions([...questions, { name, options }]);
   }
   const handleQuestionnaireCreation = () => {
-    console.log('tal2', title, questions);
     createQuestionnaire(title, questions)
   }
-  console.log('questions', questions);
+
   return (
     <Card className="questionnaire-wrapper" variant="outlined">
       <CardContent className="questionnaire-wrapper2" >
-        <Typography color="textSecondary" gutterBottom>
-            Create a Questionnaire 
-          </Typography>
+        <Typography color="textSecondary" gutterBottom>Create a Questionnaire </Typography>
         <Title onTitleChange={ onTitleChange }/>
         <Question submitQuestion={ (name, options) => submitQuestion(name, options) } questionNum={ questions.length }/>
-        {/* <button onClick={ addQuestion }>Add question</button> */}
-
-        <br />
         <Button variant="contained" color="primary" onClick={ handleQuestionnaireCreation }>Submit</Button>
-        {/* <h1>title: { title }</h1> */}
       </CardContent>
     </Card>
   );
 }
 
-const mapStateToProps = ({
-  questionnaire
-}) => ({
-  // dummy: questionnaire.dummy
-});
-
 const mapDispatchToProps = dispatch => ({
   createQuestionnaire: (title, questions) => dispatch(createQuestionnaireAction(title, questions))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Questionnaire);
+export default connect(null, mapDispatchToProps)(Questionnaire);

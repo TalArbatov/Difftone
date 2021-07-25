f709155870de46a2c9bed26a05754bc5a6c6cd58
Tal Arbatov <talarbatov98@gmail.com>
2021-07-26 02:57:53 +0300

    missed saved file

--- src/components/CreateQuestionnaire/Title.js
+++ src/components/CreateQuestionnaire/Title.js
@@ -3,6 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField';
 
+// TODO: input validation
 const Title = ({
   onTitleChange
 }) => {
